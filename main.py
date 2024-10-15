9555e345d821ac20c44615587b6477fd3e0a4d1c
shhw7389 <87451570+shhw7389@users.noreply.github.com>
2024-10-15 10:10:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ dingyu=sorted({n for n in dingyu if len(n)>0})
 #日志记录
 f_log=open('log.txt','a')
 
-for code in tqdm(dingyu,"搜索股票"):
+list1=[]
+for code in tqdm(dingyu,"搜索条件一三四"):
 
     #条件四：4十大股东持股总计超过40%。
     holders=top10.gettop10holder(code)
@@ -34,17 +35,6 @@ for code in tqdm(dingyu,"搜索股票"):
     #流通市值逻辑判断
     if f117<30*10000*10000:        continue
 
-    #条件二：股价目前在250均线之上。
-    o=stockrealtime.stock_realtime([code])[0]
-    f2=float(o['f2']) #当前价格
-    f_log.write(f'{datetime.datetime.now()} 当前价格: {code} {f2}\n')
-
-    l=candle.get_all_data(code)[:-250]
-    l=[float(i[1]) for i in l]
-    avg250= sum(l)/len(l) #250日均价
-    f_log.write(f'{datetime.datetime.now()} 250均价: {code} {avg250}\n')
-    #逻辑判断
-    if f2<avg250:continue
     
     #条件一：历史最高价与这个价格之后的最低价之间跌幅超过90%。
     l=candle.get_all_data(code)
@@ -57,7 +47,23 @@ for code in tqdm(dingyu,"搜索股票"):
     f_log.write(f'{datetime.datetime.now()} 之后的历史最低: {code} {min_}\n')
     #逻辑判断
     if min_>max_*0.1:continue
+
+    list1.append(code)
     
+for code in tqdm(list1,"搜索条件二"):
+
+    #条件二：股价目前在250均线之上。
+    o=stockrealtime.stock_realtime([code])[0]
+    f2=float(o['f2']) #当前价格
+    f_log.write(f'{datetime.datetime.now()} 当前价格: {code} {f2}\n')
+
+    l=candle.get_all_data(code)[:-250]
+    l=[float(i[1]) for i in l]
+    avg250= sum(l)/len(l) #250日均价
+    f_log.write(f'{datetime.datetime.now()} 250均价: {code} {avg250}\n')
+    #逻辑判断
+    if f2<avg250:continue
+
     with open('result.txt','a') as f:
         f.write(f'{datetime.datetime.now()} {code} 满足条件\n')
     print(code,'满足条件')
